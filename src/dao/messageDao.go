9aafb6f33cf6e40373c267d942d418cf2d61d0f6
ElectRIC_dll <2636422098@qq.com>
2024-03-09 20:22:39 +0800

    摒弃了projID参数

--- src/dao/messageDao.go
+++ src/dao/messageDao.go
@@ -3,9 +3,9 @@ package dao
 import . "campfire/entity"
 
 type MessageDao interface {
-	AddMessageRecord(projID uint, campID uint, msg ...Message) error
+	AddMessageRecord(campID uint, msg ...Message) error
 
-	PullCampMessageRecord(projID uint, campID uint, beginMessageID uint, msgCount uint) ([]Message, error)
+	PullCampMessageRecord(campID uint, beginMessageID uint, msgCount uint) ([]Message, error)
 
-	MessageRecord(projID uint, campID uint, msgID uint) (Message, error)
+	MessageRecord(campID uint, msgID uint) (Message, error)
 }
