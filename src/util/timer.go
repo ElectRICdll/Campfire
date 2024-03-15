2425f2b71e3c04d3229442daa6a4496f1b91c4de
ElectRIC_dll <2636422098@qq.com>
2024-03-15 10:39:16 +0800

    计时器实现

---
+++ src/util/timer.go
@@ -0,0 +1,38 @@
+package util
+
+import "time"
+
+type Timer struct {
+	duration  time.Duration
+	remaining time.Duration
+	timer     *time.Timer
+	isRunning bool
+}
+
+func (t *Timer) Start(callback func()) {
+	if !t.isRunning {
+		t.timer = time.NewTimer(t.remaining)
+		go func() {
+			<-t.timer.C
+			t.isRunning = false
+			callback()
+		}()
+		t.isRunning = true
+	}
+}
+
+func (t *Timer) Stop() {
+	if t.isRunning {
+		t.timer.Stop()
+		t.isRunning = false
+	}
+}
+
+func (t *Timer) Reset() {
+	t.Stop()
+	t.remaining = t.duration
+}
+
+func (t *Timer) String() string {
+	return t.remaining.String()
+}
