c8ce0af1413c937d35cabdb1525cfff31fc7d405
NightDebug <61863756+NightDebug-H@users.noreply.github.com>
2024-03-18 14:10:17 +0800

    Add files via upload

--- src/cache/testCache.go
+++ src/cache/testCache.go
@@ -28,6 +28,44 @@ func GetUserFromCache(userID int) (bool, User) {
 	return false, User{}
 }
 
+var (
+	TaskCacheByUserID *cache.Cache
+)
+
+func TaskInitByUserID() {
+	TaskCacheByUserID = cache.New(30*time.Minute, 60*time.Minute) // 设置缓存，过期时间为 30 分钟，清理间隔为 60 分钟
+}
+
+func StoreTaskInCacheByUserID(user User, task Task) {
+	TaskCacheByUserID.Set(fmt.Sprintf("user:%d", user.ID), task, cache.DefaultExpiration)
+}
+
+func GetTaskFromCacheByUserID(userID int) (bool, Task) {
+	if task, found := userCache.Get(fmt.Sprintf("user:%d", userID)); found {
+		return true, task.(Task)
+	}
+	return false, Task{}
+}
+
+var (
+	ProjectCache *cache.Cache
+)
+
+func ProjectInitBy() {
+	ProjectCache = cache.New(30*time.Minute, 60*time.Minute) // 设置缓存，过期时间为 30 分钟，清理间隔为 60 分钟
+}
+
+func StoreProjectInCacheByUserID(user User, project Project) {
+	ProjectCache.Set(fmt.Sprintf("user:%d", user.ID), project, cache.DefaultExpiration)
+}
+
+func GetProjectFromCacheByUserID(userID int) (bool, Project) {
+	if project, found := userCache.Get(fmt.Sprintf("user:%d", userID)); found {
+		return true, project.(Project)
+	}
+	return false, Project{}
+}
+
 func GetLastReadMsgIDFromCache(userID int) int {
 	if found, user := GetUserFromCache(userID); found {
 		return user.LastMsgID
