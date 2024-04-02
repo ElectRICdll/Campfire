f4dbb5d40654db7ec0e25adba050e37a17981158
ElectRICdll <2636422098@qq.com>
2024-04-02 13:23:12 +0800

    Update userDao.go

--- src/dao/userDao.go
+++ src/dao/userDao.go
@@ -68,8 +68,8 @@ func (d userDao) TasksOfUser(userID uint) ([]Task, error) {
 func (d userDao) CampsOfUser(userID uint) ([]Camp, error) {
 	var camps []Camp
 
-	if err := d.db.Preload("Owner").Preload("Members", d.db.Where(&Member{UserID: userID})).
-		Where(&Camp{IsPrivate: false}).Model(&Camp{}).
+	if err := d.db.Preload("Owner").Preload("Members", d.db.Where(&Member{UserID: userID})).Model(&Camp{}).
+		Where(&Camp{IsPrivate: false}).
 		Find(&camps).Error; err != nil {
 		return nil, err
 	}
@@ -80,8 +80,8 @@ func (d userDao) CampsOfUser(userID uint) ([]Camp, error) {
 func (d userDao) PrivateCampsOfUser(userID uint) ([]Camp, error) {
 	var camps []Camp
 
-	if err := d.db.Preload("Members", d.db.Where(&Member{UserID: userID})).
-		Where(&Camp{IsPrivate: true}).Model(&Camp{}).
+	if err := d.db.Preload("Members", d.db.Where(&Member{UserID: userID})).Model(&Camp{}).
+		Where(&Camp{IsPrivate: true}).
 		Find(&camps).Error; err != nil {
 		return nil, err
 	}
