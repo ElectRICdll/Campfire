5d52913d3de074431be68d86a8effc64984df7b4
ElectRICdll <2636422098@qq.com>
2024-04-02 15:38:29 +0800

    Update campDao.go

--- src/dao/campDao.go
+++ src/dao/campDao.go
@@ -58,7 +58,7 @@ func (d *campDao) CampInfo(campID uint, with ...string) (Camp, error) {
 
 	for _, value := range with {
 		if value == "MessageRecords" {
-			db = db.Preload(value).Order("timestamp desc").Limit(50)
+			db = db.Preload(value).Order("messages.timestamp desc").Limit(50)
 			continue
 		}
 		db = db.Preload(value)
