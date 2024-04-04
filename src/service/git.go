fdce27541d63c6a96d4782b70587d9fbe206e1c4
ElectRICdll <2636422098@qq.com>
2024-04-04 22:29:06 +0800

    Update git.go

--- src/service/git.go
+++ src/service/git.go
@@ -189,9 +189,9 @@ func (g *gitService) Branches(projID uint, path string) ([]entity.Branch, error)
 		var ref = entity.Branch{
 			ProjID: projID,
 		}
-		if reference.Type() != plumbing.InvalidReference {
+		if reference.Type() == plumbing.SymbolicReference {
 			splits := strings.Split(reference.Target().String(), "/")
-			ref.Name = splits[1]
+			ref.Name = splits[2]
 			if ref.Name == "main" {
 				ref.IsMain = true
 			} else {
